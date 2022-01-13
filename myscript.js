a2d23a454b99c343247951c49da5b8f087a94f18
AntonyMorgante <antonymorgante1@gmail.com>
2022-01-13 14:04:43 +0100

    message in h1 and img

---
+++ myscript.js
@@ -0,0 +1,14 @@
+let title = new Vue({
+    el: `#title`,
+    data: {
+        message: `Prova`,
+    }
+})
+
+let image = new Vue({
+    el: `#title-image`,
+    data: {
+        imageurl:"https://elogioallafollia.altervista.org/wp-content/uploads/2020/06/zzz-7.jpg",
+        imagename: "Paolino Paperino"
+    }
+})
